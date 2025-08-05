be7b751c07d664c54fb845aeb6b5ddf8c02648d1
Mohd Arish Khan <writetoarish7@gmail.com>
2025-08-06 01:18:15 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,6 @@ app.use(cors({
   ],
   credentials: true, 
 }));
-app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
